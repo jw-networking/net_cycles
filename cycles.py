e991b7acb0f5c7f0a08346ec344d9f561bdf523e
JoshWalker <me@joshwalker.tech>
2019-07-04 09:16:25 +0100

    fixed shebang

--- cycles.py
+++ cycles.py
@@ -1,4 +1,4 @@
-#!/bin/pyhton3
+#!/bin/python3
 #lightcycles game over lan
 
 #~~~~~~~~~~~~~~~~
@@ -8,8 +8,29 @@
 import curses
 
 #~~~~~~~~~~~~~~~~
+# static global variables
+#~~~~~~~~~~~~~~~~
+
+borders=('|','|','-','-','/','\\','\\','/')
+
+#~~~~~~~~~~~~~~~~
 # classes
 #~~~~~~~~~~~~~~~~
+
+class Player():
+    def __init__(self,name):
+        self.name=name
+
+class Net_Player(Player):
+    def __init__(self):
+        name="net"
+        super().__init__(name)
+
+
+
+class Board():
+    def __init__(self):
+        pass
 
 class Game():
     def __init__(self):
@@ -17,21 +38,26 @@ class Game():
 
 
 #~~~~~~~~~~~~~~~~
-# local methods
+# methods
 #~~~~~~~~~~~~~~~~
 
 #setup the curses environment
 def init_curses(stdscr):
     stdscr.clear()
-    size=(curses.LINES-2,curses.COLS-2)
+    size=(80,80)
     window=stdscr.subwin(*size,1,1)
-    window.nodelay(1)
     return window
 
+def draw_Window(window,game):
+    window.erase()
+    window.border(*borders)
+
+    window.refresh()
 
 #main method ran in the curses wrapper
 def main(stdscr):
     window=init_curses(stdscr)
+    window.refresh()
 
 #interactive curses if ran from comandline
 if __name__ == "__main__":
